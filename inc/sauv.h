fcb7c0d92544bbdc35bbce09c51ed49a667ff0ed
MrAntotoal <MrAntotoal26@gmail.com>
2018-12-15 00:43:14 +0100

    les lecture de sauvegarde (je peux pas les tester donc dit moi )

--- inc/sauv.h
+++ inc/sauv.h
@@ -1,10 +1,13 @@
 #ifndef _SAUV_H
 #define _SAUV_H
-#include "table_declaration.h"
+#include "analyse_sementique.h"
 /*il faut onner les autres */
 
 int sauvegarder_arbre(type_arbre *a,char * chemin);
 int sauvegarde_tables(char *chemin);
+void charger_table_hash(FILE *f1);
+type_arbre * lire_arbre(FILE *f1);
+int erreur_fichier();
 
 
 #endif
